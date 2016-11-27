8fb89e63e29fdc38a6bd71e626230fa05d95a308
Ha Binh <lethihabinh@gmail.com>
2016-11-27 16:31:12 +0700

    HB

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// my-skills Script 
+var percent;
+var bar;
+bar = document.querySelectorAll('.bar');
+percent = document.querySelectorAll('.bar-percent');
+
+percent.forEach(function(item,index){
+	var content = item.parentElement.nextElementSibling.innerHTML;
+	var ball = item.nextElementSibling;
+	var len = (Number(content[0])*10+Number(content[1]))/100;
+	len *= bar[index].clientWidth;
+	len -= ball.clientWidth/2;
+	item.style.width = content;
+	ball.style.left = len+'px';
+});
+
+var link = document.querySelectorAll('a[href="#"]');
+function PreventDefault(e){
+	e.preventDefault();
+}
+link.forEach(function(item,index){
+	item.addEventListener('click',PreventDefault);
+});
+
+//Menu Toggle Script 
+$(document).ready(function(){
+    $("#sidebar-wrapper ul").height($('#wrapper').height()-$('#portrait').height());
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $content = $("#header");//document.getElementById("content-header");
+        $txt = $content.html();
+        $("#wrapper").addClass("toggled");
+        $content.prepend('<a class="btn glyphicon glyphicon-menu-hamburger" href="#"></a>');
+        $content.find("h5:eq(1)").hide();
+        $content.find("h1:eq(0)").addClass("name").text("Al Rayhan");
+        $content.find("h5:eq(0)").addClass("name").text("#Ui/ux Expert   |   #Graphic Designer");
+    });
+
+    $(document).on('click','a.btn',function(e){
+        e.preventDefault();
+        $("#wrapper").removeClass("toggled");
+        $content.html($txt);
+    });
+})
